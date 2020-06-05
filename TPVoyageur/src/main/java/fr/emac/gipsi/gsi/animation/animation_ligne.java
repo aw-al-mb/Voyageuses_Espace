b093e080815cade0897ce1a84de180a45b8014a2
Alicia <alicia.laroche@mines-albi.fr>
2020-06-05 14:01:15 +0200

    tralala

---
+++ TPVoyageur/src/main/java/fr/emac/gipsi/gsi/animation/animation_ligne.java
@@ -0,0 +1,11 @@
+package fr.emac.gipsi.gsi.animation;
+
+public class animation_ligne extends AbstractAnimation{
+
+	@Override
+	public void runAnimation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
