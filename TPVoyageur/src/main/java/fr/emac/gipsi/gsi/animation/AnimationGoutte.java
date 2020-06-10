282c48dbebb56ebd444e878ec1f1d3ed11143236
aw-al-mb* <alice.weber@mines-albi.fr>
2020-06-10 12:39:54 +0200

    Rectification

--- TPVoyageur/src/main/java/fr/emac/gipsi/gsi/animation/AnimationGoutte.java
+++ TPVoyageur/src/main/java/fr/emac/gipsi/gsi/animation/AnimationGoutte.java
@@ -22,27 +22,27 @@ public class AnimationGoutte extends AbstractAnimation {
 			ecranInt.updateColorByXY(n/2, n/2, ecranFin.getColorByXY(n/2, n/2));
 			ecranInt.updateColorByXY(n/2, n/2 - 1, ecranFin.getColorByXY(n/2, n/2 - 1));
 			
-			for (int i = 1; i < n/2 -1 ; i++) 
+			for (int i = 1; i <= n/2 -1 ; i++) 
 			{
 				System.out.println("startSend");
 				this.showScreen(ecranInt);
 				
-				for (int j = n/2 - 1 - i; j < n/2 + i; i++)
+				for (int j = n/2 - 1 - i; j <= n/2 + i; j++)
 				{
 					ecranInt.updateColorByXY(n/2 - 1 - i, j, ecranFin.getColorByXY(n/2 - 1 - i, j));
 				}
 				
-				for (int k = n/2 - i; k< n/2 + i; k++)
+				for (int k = n/2 - i; k <= n/2 + i; k++)
 				{
 					ecranInt.updateColorByXY(k, n/2 + i, ecranFin.getColorByXY(n/2 + i,  k));
 				}
 				
-				for (int g = n/2 + i - 1; g> n/2 - 1 - i; g--)
+				for (int g = n/2 + i - 1; g>= n/2 - 1 - i; g--)
 				{
 					ecranInt.updateColorByXY(n/2 + i, g, ecranFin.getColorByXY(n/2 + i,  g));
 				}
 				
-				for (int h = n/2 + i - 1; h> n/2 - i; h--)
+				for (int h = n/2 + i - 1; h>= n/2 - i; h--)
 				{
 					ecranInt.updateColorByXY(h, n/2 - 1 - i, ecranFin.getColorByXY(h, n/2 - 1 - i));
 				}
